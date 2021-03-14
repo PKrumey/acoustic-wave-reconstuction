1b56c98ed34720c940a41a52e6c46f648ae656cc
PKrumey <80647646+PKrumey@users.noreply.github.com>
2021-03-15 00:46:48 +0100

    Update calcCRC.m

--- code/calcCRC.m
+++ code/calcCRC.m
@@ -1,6 +1,7 @@
 function convCRC = calcCRC(pulse,theta,time)
-    %% calcCRC: This function calculates the convoluted calculated
-    %             rocking curve
+    %% calcCRC: This function calculates the convoluted rocking curve
+
+    %calculate rocking curve depending on polarization flag in constants
     if constants.Pol_flag == 1 || constants.Pol_flag == 0
         CRC = calcCRCPol(pulse,theta,time,constants.Pol_flag);
     else
@@ -9,7 +10,7 @@ function convCRC = calcCRC(pulse,theta,time)
         CRC = constants.Pol_flag*CRCspol+(1-constants.Pol_flag)*CRCppol;
     end
 
-
+    % convolute the calculated rocking curve
     ConvolutionFile =   [theta CRC];
     [convCRC, ~] = substrate_convolution_v5_GaAs111sm(ConvolutionFile);
     convCRC(convCRC == 0) = 1e-9;        
@@ -18,29 +19,32 @@ end
 function CRC = calcCRCPol(pulse,theta,time,Pol_flag)
     %% calcCRC: This function calculates the rocking curve with s or p
     %               Polarisation
+    
+    % calculate input parameters
     subs = XRD_inputsV8_m_s(constants.F0_Re, constants.F0_Im, ...
         constants.Fpsi_Re, constants.Fpsi_Im, constants.DWF, Pol_flag, ...
         constants.Theta_B, constants.lambda, constants.a, constants.R_elctron);
 
+    % build strain array
     strain = buildStrain(pulse,time);
 
     % prepare parfor variables; note: cells are used to provide 'sliced
     % variables' for parallelization
-    NbrTimeSteps    = length(time);
-    N               = length(theta);
-    RCcell          =   cell(N,1);	% cell for RCs
+    NbrTimeSteps    = length(time);             %number of times
+    N               = length(theta);            % number of angles
+    RCcell          =   cell(N,1);	            % cell for RCs
     Scell           =   cell(1, NbrTimeSteps);	% put strain in a cell structure for sliced variable use in parfor
 
     for i = 1:NbrTimeSteps                      % find the appropriate column of S and put it into the cells
         Scell{i} = strain(i, :);
     end
 
-    parfor j = 1:N      
-        RCcell{j}  =   zeros(1,NbrTimeSteps);   % 
+    parfor j = 1:N                              % loop over angles
+        RCcell{j}  =   zeros(1,NbrTimeSteps);   % cell that holds rocking curves
 
-        for tt = 1:NbrTimeSteps
-            S_z        =   Scell{tt}';      %#ok % pick correct strain 
-            ang        =   theta(j);                         
+        for tt = 1:NbrTimeSteps                 % loop over times
+            S_z        =   Scell{tt}';          % pick correct strain 
+            ang        =   theta(j);            % pick correct angle             
 
             % solve the DXRD equation                    % integration borders, initial conditions, options,                                              
             [A, X] = DXRD_ode23_C_mex(ang, subs.k, subs.g_c, subs.y_a, subs.y_c, subs.BraggAngle, subs.A_T, constants.dSubs, S_z)
@@ -48,7 +52,7 @@ function CRC = calcCRCPol(pulse,theta,time,Pol_flag)
         end
     end
 
-    CRC  = zeros(N, NbrTimeSteps);       % put the RCs into one matrix
+    CRC  = zeros(N, NbrTimeSteps);              % put the RCs into one matrix
     for i = 1:N  
         CRC(i,:)     = RCcell{i};
     end
@@ -58,10 +62,13 @@ function strain = buildStrain(pulse,time)
     %% buildStrain: This function takes a bipolar pulse and creates a strain
     %                 matrix for the given time vector
 
+    % create pulse train from pulses and with reflectivity R
     fullPulseTrain = [constants.R^7*pulse constants.R^6*pulse constants.R^5*pulse constants.R^4*pulse constants.R^3*pulse constants.R^2*pulse constants.R*pulse pulse];
-  
-    S       =   zeros(length(time), 2000);
+ 
+    % initialize strain array
+    S       =   zeros(length(time), 2000);  
 
+    % calculate strain out of pulse train
     for i = 1:length(time)
         z_t = round(time(i)*1e-12*constants.svSubs/1e-9);
         if length(fullPulseTrain) > z_t
