6513f4c0efaa82e734f1e842be48a4df7359c501
PKrumey <80647646+PKrumey@users.noreply.github.com>
2021-03-15 01:59:01 +0100

    Update calcCRC.m

--- code/calcCRC.m
+++ code/calcCRC.m
@@ -1,10 +1,27 @@
 function convCRC = calcCRC(pulse,theta,time)
-    %% calcCRC: This function calculates the convoluted rocking curve
-
-    %calculate rocking curve depending on polarization flag in constants
+% ************************************************************************
+% calcCRC:  This function calculates the convoluted rocking curve           
+% ########################################################################
+% INPUTS
+% ########################################################################
+% pulse:    Vector containing the bipolare strain pulse
+% theta:    Vector containing the angles for convCRC
+% time:     Vector containing the time points for convCRC
+%#########################################################################
+% OUTPUT
+% ########################################################################
+% convCRC:  Array containing the calculated convoluted rocking curves
+%           [theta,time]
+% ########################################################################
+% Philipp Krumey
+% 15-03-2021, University of Duisburg-Essen
+% *************************************************************************
+    % calculate rocking curve depending on polarization flag in constants
     if constants.Pol_flag == 1 || constants.Pol_flag == 0
+        % calulate rocking curves for polarized x-rays
         CRC = calcCRCPol(pulse,theta,time,constants.Pol_flag);
     else
+        % calulate rocking curves for un or partly polarized x-rays
         CRCspol = calcCRCPol(pulse,theta,time,0);
         CRCppol = calcCRCPol(pulse,theta,time,1);
         CRC = constants.Pol_flag*CRCspol+(1-constants.Pol_flag)*CRCppol;
@@ -17,10 +34,25 @@ function convCRC = calcCRC(pulse,theta,time)
 end
 
 function CRC = calcCRCPol(pulse,theta,time,Pol_flag)
+    % ************************************************************************
     %% calcCRC: This function calculates the rocking curve with s or p
     %               Polarisation
+    % ########################################################################
+    % INPUTS
+    % ########################################################################
+    % pulse:    Vector containing the bipolare strain pulse
+    % theta:    Vector containing the angles for convCRC
+    % time:     Vector containing the time points for convCRC
+    % Pol_flag  This flag determins the polarization of the X-Ray 
+    %           [0 means s pol, 1 means p pol
+    %#########################################################################
+    % OUTPUT
+    % ########################################################################
+    % CRC:  Array containing the calculated rocking curves
+    %       [theta,time]    
+    % *************************************************************************
     
-    % calculate input parameters
+    % calculate parameters for DXRD
     subs = XRD_inputsV8_m_s(constants.F0_Re, constants.F0_Im, ...
         constants.Fpsi_Re, constants.Fpsi_Im, constants.DWF, Pol_flag, ...
         constants.Theta_B, constants.lambda, constants.a, constants.R_elctron);
@@ -46,7 +78,7 @@ function CRC = calcCRCPol(pulse,theta,time,Pol_flag)
             S_z        =   Scell{tt}';          % pick correct strain 
             ang        =   theta(j);            % pick correct angle             
 
-            % solve the DXRD equation                    % integration borders, initial conditions, options,                                              
+            % solve the DXRD equation                                                                 
             [A, X] = DXRD_ode23_C_mex(ang, subs.k, subs.g_c, subs.y_a, subs.y_c, subs.BraggAngle, subs.A_T, constants.dSubs, S_z)
             RCcell{j}(tt)	=   (X(length(A),1).^2 + X(length(A),2).^2)';	%calculation of the X-ray reflectivity
         end
@@ -59,8 +91,20 @@ function CRC = calcCRCPol(pulse,theta,time,Pol_flag)
 end
 
 function strain = buildStrain(pulse,time)
+    % ************************************************************************
     %% buildStrain: This function takes a bipolar pulse and creates a strain
     %                 matrix for the given time vector
+    % ########################################################################
+    % INPUTS
+    % ########################################################################
+    % pulse:    Vector containing the bipolare strain pulse
+    % time:     Vector containing the time points for strain
+    %#########################################################################
+    % OUTPUT
+    % ########################################################################
+    % strain:   Array containing the strain pulsetrains for all times 
+    %           [time,pulsetrain]    
+    % *************************************************************************
 
     % create pulse train from pulses and with reflectivity R
     fullPulseTrain = [constants.R^7*pulse constants.R^6*pulse constants.R^5*pulse constants.R^4*pulse constants.R^3*pulse constants.R^2*pulse constants.R*pulse pulse];
@@ -68,7 +112,7 @@ function strain = buildStrain(pulse,time)
     % initialize strain array
     S       =   zeros(length(time), 2000);  
 
-    % calculate strain out of pulse train
+    % build strain array out of pulse train
     for i = 1:length(time)
         z_t = round(time(i)*1e-12*constants.svSubs/1e-9);
         if length(fullPulseTrain) > z_t
