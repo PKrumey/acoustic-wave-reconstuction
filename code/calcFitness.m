7581fc326b399251d7abf27230d12ee56d2c35b6
PKrumey <80647646+PKrumey@users.noreply.github.com>
2021-03-15 01:39:36 +0100

    Update calcFitness.m

--- code/calcFitness.m
+++ code/calcFitness.m
@@ -1,24 +1,39 @@
 function fitness = calcFitness(CRC, MRC,theta,filter)
-    %%%% calcFitness: Calculates the fitness value of the object            
-    %                 normalize MRC and CRC to increase effect of 
-    %                 side peaks
+% ************************************************************************
+% calcFitness:    Calculates the fitness value of the object            
+%                 normalize MRC and CRC to increase effect of 
+%                 side peaks
+% ########################################################################
+% INPUTS
+% ########################################################################
+% CRC:      matrix of the calculated rocking curve in the form 
+%           [theta,time]
+% MRC:      measured rocking curve in the form [theta,time]
+% theta:    Vector containing the angles of the MRC
+% filter:   Vector containing the angular area, that should be taken into 
+%           acount to calculate the fintess 
+%           [anglemin1,anglemax1,anglemin2,anglemax2]
+%#########################################################################
+% OUTPUT
+% ########################################################################
+% fitness:  positive number holding the deveation between CRC and MRC
+% ########################################################################
+% Philipp Krumey
+% 15-03-2021, University of Duisburg-Essen
+% *************************************************************************
 
+    %calculate the indices, corresponding to anglemin1,anglemax1,anglemin2,
+    %anglemax2
     [~, idx1] = min(abs(theta - filter(1)));
     [~, idx2] = min(abs(theta - filter(2)));
     [~, idx3] = min(abs(theta - filter(3)));
     [~, idx4] = min(abs(theta - filter(4))); 
 
-    filtMRC1 = MRC(idx1:idx2,:);
-    filtMRC2 = MRC(idx3:idx4,:);
-    for i = 1:size(MRC,2)
-        filtMRC1(:,i) = filtMRC1(:,i);%smooth(filtMRC1(:,i));
-        filtMRC2(:,i) = filtMRC2(:,i);%smooth(filtMRC2(:,i));
-    end
-    filtMRC = [filtMRC1; filtMRC2];
+    %filter MRC and save filtered data in filtMRC
+    filtMRC = MRC([idx1:idx2 idx3:idx4],:);
 
+    %filter CRC and save filtered data in filtCRC
     filtCRC = CRC([idx1:idx2 idx3:idx4],:);
-
-    %filtMRC0 = [MRC0(idx1:idx2); MRC0(idx3:idx4)];%[smooth(MRC0(idx1:idx2)); smooth(MRC0(idx3:idx4))];
 
     %recalculating deveation into fitness
     fitness = logNorm(filtMRC, filtCRC);
