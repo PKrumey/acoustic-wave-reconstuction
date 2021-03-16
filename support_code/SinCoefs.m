bb096863d1523589efa7575302bf2016a6d952dd
PKrumey <80647646+PKrumey@users.noreply.github.com>
2021-03-16 15:53:56 +0100

    Update SinCoefs.m

--- support_code/SinCoefs.m
+++ support_code/SinCoefs.m
@@ -1,5 +1,23 @@
 function Coefs = SinCoefs(f,maxk,halfL)
-%SINCOEFS Calculates maxk Sin Fourier Coefficients of f
+% ************************************************************************
+%SINCOEFS Calculates a number of sine fourier coefficients for the 
+%         function f
+% ########################################################################
+% INPUTS
+% ########################################################################
+% f:      a function of the length 2*halfL+1
+% maxk:   the number of sine coefficients to be calculated plus 1:
+%         0:maxk (sine coefficient for k=0 is always 0)
+% halfL:  half the length of the function f
+%#########################################################################
+% OUTPUT
+% ########################################################################
+% Coefs:  calculated sine coefficients
+% ########################################################################
+% Philipp Krumey
+% 14-03-2021
+% University of Duisburg-Essen
+% ************************************************************************
 x = -halfL:halfL;
 k = 0:maxk;
 Coefs = trapz(x,f'.*sin(pi*k.*x'/halfL)/halfL);
