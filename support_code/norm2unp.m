26228398c485309a9b08b30aac478dc10e7d255c
PKrumey <80647646+PKrumey@users.noreply.github.com>
2021-03-16 23:32:38 +0100

    Update norm2unp.m

--- support_code/norm2unp.m
+++ support_code/norm2unp.m
@@ -1,12 +1,26 @@
 function RCnorm = norm2unp(RC)
-% This function normalizes the RC matrix to the maximum of the unpumped RC,
+% ************************************************************************
+%%Norm2unp This function normalizes the RC matrix to the maximum of the unpumped RC,
 % which is supposed to be in the first column.
-% mini = min(RC(:));
-% RCnorm = RC - mini;
+% ########################################################################
+% INPUTS
+% ########################################################################
+% RC:   rocking curves to be normalized
+%#########################################################################
+% OUTPUT
+% ########################################################################
+% RCnorm: normalized rocking curves
+% ########################################################################
+% Philipp Krumey
+% 14-03-2021
+% University of Duisburg-Essen
+% ************************************************************************
 
+%calculate the maximum of the unpumped rocking curve
 maxi = max(RC);
 maxOFF = maxi(1);
 
+%norm all rocking curves to the maximum of the unpumped rocking curve
 RCnorm = RC / maxOFF;
 end
 
