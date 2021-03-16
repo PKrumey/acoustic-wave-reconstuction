7421aa42320193262892ac5a2a0a47406c9e191e
PKrumey <80647646+PKrumey@users.noreply.github.com>
2021-03-16 23:40:42 +0100

    Update start.m

--- start.m
+++ start.m
@@ -1,16 +1,17 @@
 %START routine to starts optimization algorithm
 %   This is the start script for running a optimization algorithm to retrieve the
 %   strain pulse out of the measured rocking curves for different time
-%   delays.
+%   delays. Here the main function of the algorithm is called
 % ########################################################################
 % Philipp Krumey
 % 14-03-2021, University of Duisburg-Essen
 % *************************************************************************
 
 %add paths from folder
-addpath(genpath('code'));
+addpath(genpath('reconstruction'));
 addpath(genpath('support_code'));
 addpath(genpath('workspace'));
+addpath(genpath('XRD calculation'));
 
 %start main routine
 main('workspaceTiGaAs111Full.mat',200,[-0.6 -0.1 0.1 0.6],'none');
