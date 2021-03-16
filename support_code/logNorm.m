d1dfdd342d1d67cbbfed7c36b0a372703770bab7
PKrumey <80647646+PKrumey@users.noreply.github.com>
2021-03-16 23:30:04 +0100

    Update logNorm.m

--- support_code/logNorm.m
+++ support_code/logNorm.m
@@ -1,9 +1,32 @@
-function fitness = logNormV2(MRC, CRC)
-%logNorm: Normalization is done by taking the logarithm of both matrices
+function fitness = logNorm(MRC, CRC)
+% ************************************************************************
+%logNorm: calculates the fitness between two rocking curves by taking
+%         the sum over the squared deveation of the logarithmic rocking
+%         curves
+% ########################################################################
+% INPUTS
+% ########################################################################
+% MRC:          The measured rocking curve [angles,times]
+% CRC:          The calculated rocking curvs [angles,times]
+%#########################################################################
+% OUTPUT
+% ########################################################################
+% fitness:      a single value > 0 expressing the fiteness between MRC
+%               and CRC
+% ########################################################################
+% Philipp Krumey
+% 14-03-2021
+% University of Duisburg-Essen
+% ************************************************************************
+
+        %calculate logarithm of MRC, and CRC
         normMRC = log(MRC+1e-4);
         normCRC = log(CRC+1e-4);
+        
+        %determine the number of angles and time points for normalization
         [thetas,times] = size(CRC);
-        % fitness is sum of deviation squares
+        
+        % fitness is sum of normed deviation squares
         fitness = sum(sum((normMRC - normCRC).^2)/thetas)/times;
         
 end
